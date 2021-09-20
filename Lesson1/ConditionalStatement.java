2797d2a1d82d64a1dedae289e23549ffef1a4cbd
VladRidun <ridunvv@gmail.com>
2021-09-20 22:12:55 +0400

    first lesson

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -4,6 +4,7 @@ public class ConditionalStatement {
         if (a > 20) {
             System.out.println("Возраст больше 20 лет!");    
         }
+
         char m = 'M';
         char s = 'M';
         if (s == m) {
@@ -12,6 +13,7 @@ public class ConditionalStatement {
         if  (s != m) {
             System.out.println("Пол женский!");
         }
+
         double x = 1.80;
         double h = 1.71;
         if (h < x) {
@@ -19,15 +21,16 @@ public class ConditionalStatement {
         } else {
             System.out.println("Рост больше 1.80");
         }
-        char n = 'V';
-        char w = 'M';
-        char i = 'I';
+
+        char nameUser = 'V';
+        char nameM = 'M';
+        char nameI = 'I';
         if (n == w) {
             System.out.println("имя начинается с буквы М");
         } else if (n == i) {
             System.out.println("имя начинается с буквы I");
         } else { 
             System.out.println("What it is your name?");
-            }
+        }
         }
     }
