9a0b066590193569cc421cf445fe8fea16813a13
VladRidun <ridunvv@gmail.com>
2021-09-20 21:35:29 +0400

    second lesson

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -4,19 +4,25 @@ public class Wolf {
    public String colour;
    public int age;
    public int weight;
+
    public void go() {
        System.out.println("Idti");
     }
+
    public void sit() {
        System.out.println("Sidet'");
     }
+
    public void run() {
        System.out.println("Begat'");
     }
+
    public void bark() {
        System.out.println("Vyt'");
     }
+
    public void hunt() {
        System.out.println("Ohotitsia");
     }
+    
 }
