38857be1718d8d3f362bab71bc68b0b59ed533fb
VladRidun <ridunvv@gmail.com>
2021-09-22 21:14:29 +0400

    Lesson1

--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -2,8 +2,7 @@ public class Unicode {
     public static void main(String[] args) {
         int startRange = 33, endRange = 126;
         for (int i = startRange; i <= endRange; i++) {
-            char symbolUnicode = (char) i;
-            System.out.println("chislo " + i + " v Unicode = " + symbolUnicode);
+            System.out.println("chislo " + i + " v Unicode = " + (char) i);
         }
     }
 }
