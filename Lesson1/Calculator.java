f083e5c17aae2ca26209e38ae6686d6bd5e3dc9e
VladRidun <ridunvv@gmail.com>
2021-09-22 21:11:01 +0400

    Lesson1

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,33 +1,33 @@
 public class Calculator {
     public static void main(String[] args) {
-// здесь меняем значение переменных a and b 
-        int a = 5;
-        int b = 2;
-        char x = '%';// для выбора  меняем значение переменной
-//переменный с операторами
-        int oper = 0;
+        // здесь меняем значение переменных a and b 
+        int firstNumber = 5;
+        int secondNumber = 2;
+        char operator = '%';// для выбора  меняем значение переменной
+        //переменный с операторами
+        int resultOperation = 0;
 
-        if (x == '+') {
-            oper = a + b;
-            System.out.println("Summa " + a + "+" + b + "=" + oper);       
-        } else if (x == '-') {
-            oper = a - b;
-            System.out.println("Raznost " + a + "-" + b + "=" + oper);
-        } else if (x == '*') {
-            oper = a * b;
-            System.out.println("Proizvedenie " + a + "*" + b + "=" + oper);    
-        } else if (x == '/') {
-            oper = a / b;
-            System.out.println("Delenie " + a + "/" + b + "=" + oper);
-        } else if (x == '%') {
-            oper = a % b;
-            System.out.println("Delenie po moduliu " + a + "%" + b + "=" + oper);
-        } else if (x == '^') {
-            int result = 1;
-            for (int i = 1; i <= b; i++) {
-                result *= a;
+        if (operator == '+') {
+            resultOperation = firstNumber + secondNumber;
+            System.out.println("Summa " + firstNumber + "+" + secondNumber + "=" + resultOperation);
+        } else if (operator == '-') {
+            resultOperation = firstNumber - secondNumber;
+            System.out.println("Raznost " + firstNumber + "-" + secondNumber + "=" + resultOperation);
+        } else if (operator == '*') {
+            resultOperation = firstNumber * secondNumber;
+            System.out.println("Proizvedenie " + firstNumber + "*" + secondNumber + "=" + resultOperation);
+        } else if (operator == '/') {
+            resultOperation = firstNumber / secondNumber;
+            System.out.println("Delenie " + firstNumber + "/" + secondNumber + "=" + resultOperation);
+        } else if (operator == '%') {
+            resultOperation = firstNumber % secondNumber;
+            System.out.println("Delenie po moduliu " + firstNumber + "%" + secondNumber + "=" + resultOperation);
+        } else if (operator == '^') {
+            int resultStepen = 1;
+            for (int i = 1; i <= secondNumber; i++) {
+                result *= firstNumber;
             }
-            System.out.println(a + " v stepeni " + b + " = " + result);
+            System.out.println(firstNumber + " v stepeni " + secondNumber + " = " + resultStepen);
         }
     }
 }
--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,22 +1,20 @@
-public class MyFirstGame {   
+public class MyFirstGame {
     public static void main(String[] args) {
-    int targetNumber = 13;
-    int playerNumber = 5;
+        int targetNumber = 13;
+        int playerNumber = 5;
 
-      System.out.println("Ugadaite chislo, kotoroe zagadal computer");
+        System.out.println("Ugadaite chislo, kotoroe zagadal computer");
 
         do { 
             if (playerNumber > targetNumber) {
                 System.out.println("Dannoe chislo bolshe togo chto zagadal computer" + playerNumber);
                 playerNumber--;
-            } else  if (playerNumber < targetNumber) {
+            } else if (playerNumber < targetNumber) {
                 System.out.println("Dannoe chislo menshe togo chto zagadal computer" + playerNumber);
                 playerNumber++;
             }
         } while (playerNumber != targetNumber);
-        
-        if (playerNumber == targetNumber) {
-                System.out.println("Vy ugadali chislo " + targetNumber);       
-        }
+            System.out.println("Vy ugadali chislo " + targetNumber);       
     }
 }
+
