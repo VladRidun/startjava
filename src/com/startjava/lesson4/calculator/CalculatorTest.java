71fd0e8a46a7a8233dcb969abfb90b25a5d3f85f
VladRidun <ridunvv@gmail.com>
2021-11-04 20:09:26 +0400

    Lesson4

--- src/com/startjava/lesson4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson4/calculator/CalculatorTest.java
@@ -6,17 +6,14 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner sc = new Scanner(System.in);
-        String choice;
+        String choice = "yes";
         do {
-            System.out.println("Введите выражение");
-            String expression = sc.nextLine();
-            System.out.println("Результат вычисления:" + calculator.calc(expression));
+            if (choice.equalsIgnoreCase("yes")) {
+                System.out.println("Введите выражение");
+                System.out.println("Результат вычисления:" + calculator.calc(sc.nextLine()));
+            }
             System.out.println("Хотите продолжить вычисления [yes/no]:");
             choice = sc.nextLine();
-            if (!choice.equalsIgnoreCase("yes") ) {
-                System.out.println("Хотите продолжить вычисления [yes/no]:");
-                choice = sc.nextLine();
-            }
         } while (!choice.equalsIgnoreCase("no"));
     }
 }
