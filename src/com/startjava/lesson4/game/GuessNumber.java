18bb6c5f6b4660be6c8775e3dffd3362050fc430
VladRidun <ridunvv@gmail.com>
2021-11-05 15:47:49 +0400

    Lesson4

--- src/com/startjava/lesson4/game/GuessNumber.java
+++ src/com/startjava/lesson4/game/GuessNumber.java
@@ -1,14 +1,15 @@
 package com.startjava.lesson4.game;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
     private Player p1;
     private Player p2;
     private int targetNumber;
+    private int count;
+    private int i;
 
-    public GuessNumber(Player player1, Player  player2) {
+    public GuessNumber(Player player1, Player player2) {
         p1 = player1;
         p2 = player2;
     }
@@ -16,34 +17,48 @@ public class GuessNumber {
     public void start() {
         Scanner sc = new Scanner(System.in);
         targetNumber = (int) (Math.random() * 101);
-      // выводится число загаданное компьютером для проверки условий вывода
-       System.out.println("Copmputer guess number : " + targetNumber);
+        // выводится число загаданное компьютером для проверки условий вывода
+        System.out.println("Copmputer guess number : " + targetNumber);
 
-        while (true) {
-            System.out.println("You  have 10 attempts!");
+
+        System.out.println("You  have 10 attempts!");
+        count = 1;
+        for (int i = 0; i < 10; i++) {
             System.out.println("Input number " + p1.getName());
-            p1.setNumber(sc.nextInt());
-            
-            if (p1.getNumber() > targetNumber) {
-                System.out.println(p1.getName() + "  intput a number " + p1.getNumber() + " bigger than the computer guessed a number");
-            } else if (p1.getNumber() < targetNumber) {
-                System.out.println(p1.getName() + "  intput a number " + p1.getNumber() + " smaller than computer guessed a number");
+            p1.setNumberArray(sc.nextInt(), i);
+            if (p1.getNumber(i) > targetNumber) {
+                System.out.println(p1.getName() + "  intput a number " + p1.getNumber(i) + " bigger than the computer guessed a number");
+            } else if (p1.getNumber(i) < targetNumber) {
+                System.out.println(p1.getName() + "  intput a number " + p1.getNumber(i) + " smaller than computer guessed a number");
             } else {
-                System.out.println("Player  " + p1.getName() + "guess number"+ p1.getNumber()+ "with");
+                System.out.println("Player  " + p1.getName() + " guess number " + targetNumber + " with " + count + " attempts");
                 break;
+            }
+            if (count == 10) {
+                System.out.println("Player  " + p1.getName() + "  don't have attempts");
             }
 
             System.out.println("Input number " + p2.getName());
-            p2.setNumber(sc.nextInt());
+            p2.setNumberArray(sc.nextInt(), i);
 
-            if (p2.getNumber() > targetNumber) {
-                System.out.println(p2.getName() + "  intput a number " + p2.getNumber() + " bigger than computer guessed a number");
-            } else if (p2.getNumber() < targetNumber) {
-                System.out.println(p2.getName() + "  intput a number " + p2.getNumber() + " smaller than computer guessed a number");
+            if (p2.getNumber(i) > targetNumber) {
+                System.out.println(p2.getName() + "  intput a number " + p2.getNumber(i) + " bigger than computer guessed a number");
+            } else if (p2.getNumber(i) < targetNumber) {
+                System.out.println(p2.getName() + "  intput a number " + p2.getNumber(i) + " smaller than computer guessed a number");
             } else {
-                System.out.println("Win player  " + p2.getName());
+                System.out.println("Player  " + p2.getName() + " guess number " + targetNumber + " with " + count + " attempts");
                 break;
             }
+            if (count == 10) {
+                System.out.println("Player  " + p2.getName() + "  don't have attempts");
+
+            }
+            count++;
         }
+        System.out.println("Player "+p1.getName() +" input : " + p1.getNumberArray(i));
+        System.out.println("Player "+p2.getName() +" input : "+ p2.getNumberArray(i));
+        p1.fillArray(i);
+        p2.fillArray(i);
     }
 }
+
--- src/com/startjava/lesson4/game/Player.java
+++ src/com/startjava/lesson4/game/Player.java
@@ -1,12 +1,12 @@
 package com.startjava.lesson4.game;
 
-import java.util.Scanner;
+import java.util.Arrays;
+
+import static java.util.Arrays.copyOf;
 
 public class Player {
     private String name;
-    private int number;
     private int[] numberArray = new int[10];
-
 
     public Player(String name) {
         this.name = name;
@@ -16,11 +16,20 @@ public class Player {
         return name;
     }
 
-    public void setNumber(int number) {
-        this.number = number; //Заполняем массив элементами, введёнными с клавиатуры
+    public void setNumberArray(int number, int i) {
+        this.numberArray[i] = number; //Заполняем массив элементами, введёнными с клавиатуры
     }
 
-    public int getNumber() {
-        return number;
+    public int getNumber(int i) {
+        return numberArray[i];
+    }
+
+    public String getNumberArray(int i) {
+        return Arrays.toString(Arrays.copyOf(numberArray, i));
+    }
+
+    public void fillArray(int i) {
+        Arrays.fill(numberArray, 0, i, 0);
     }
 }
+
